3f0cddf79704572a807a21da28932d620c87345b
Gu Kexi <gukexizzg@163.com>
2019-04-25 17:20:53 +0800

    Add data_processing.py and python_zen.py

---
+++ simlpe_data_processing/src/data_processing.py
@@ -0,0 +1,63 @@
+'''
+Created on Apr 25, 2019
+
+@author: ekexigu
+'''
+
+# string processing
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+
+print("\n\tPython\nC\n\tJava")
+
+favorite_language = " Python "
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
+
+# list processing
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[0])
+print(bicycles[2])
+print(bicycles[3])
+print(bicycles[-1])
+print(bicycles[0].title())
+
+bicycles[0] = 'bmw'
+print(bicycles)
+
+bicycles.append('trek')
+print(bicycles)
+
+del bicycles[2]
+bicycles.insert(3, 'redline')
+print(bicycles)
+
+print(bicycles.pop(1))
+bicycles.remove('bmw')
+print(bicycles)
+
+bicycles.reverse()
+print(bicycles)
+
+bicycles.sort()
+print(bicycles)
+bicycles.sort(reverse=True)
+print(bicycles)
+
+print(sorted(bicycles))
+print(bicycles)
+
+print(len(bicycles))
