b4aa8907c16752370b06bd17c6cd9e3a77447131
Gu Kexi <gukexizzg@163.com>
2019-07-29 14:59:19 +0200

    Update main.py to use the default directory.

--- create_tc/src/main.py
+++ create_tc/src/main.py
@@ -6,14 +6,12 @@ This is the main function.
 @author: ekexigu
 '''
 
-from os import path as p
 from modules import json_handling as jh
 from modules import dict_handling as dh
 from modules import create_tc_measure_points as ctmp
 
-current_path = p.dirname(__file__)                                                  #Get the current function path.
-json_operate = open(p.join(current_path, 'TC_Configuration.json'), 'r')             #Get the current json file full name and open it in 'read' mode.
-tc_operate = open(p.join(current_path, 'TC_Temp.txt'), 'w')                         #Get the current txt file full name and open it in 'write mode'.
+json_operate = open('TC_Configuration.json', 'r')             #Get the current json file full name and open it in 'read' mode.
+tc_operate = open('TC_Temp.txt', 'w')                         #Get the current txt file full name and open it in 'write mode'.
 
 ctmp.creat_comments(dh.list_from_dict(jh.load_json_file(json_operate)), tc_operate) #Use the functions in these modules to write the comments into txt file according to json file.
 
